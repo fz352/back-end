587fb3afbe005fa8b546c75f67b64ce6c218f32b
ruanmuller20 <ruanmuller20@gmail.com>
2025-07-14 22:19:09 -0300

    integração com frontend

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import { router } from './routes';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 
+app.use(cors({
+  origin: 'http://127.0.0.1:5500', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+}));
+
+app.use(cors()); 
 app.use(router);
 
 app.listen(3333, () => 
